8ef60608f3b8b87f08a79e3a2f5c59a20b4787fa
Sinu <vijay.kesanupalli@synectiks.com>
2018-01-04 22:31:27 +0530

    Alignment changes

--- src/_config/eskaysoftCommons.js
+++ src/_config/eskaysoftCommons.js
@@ -20,179 +20,184 @@
 (function () {
     "use strict";
     angular.module('com.synectiks.eskaySoft')
-	.provider("eskaySoftTranslate", ["$translateProvider",
+        .provider("eskaySoftTranslate", ["$translateProvider",
 		function ($translateProvider) {
-			var translateConfig = {
-				resourceUrl: '',
-				language: 'en',
-				fallBackLanguage: 'en',
-				features: ['']
-			};
+                var translateConfig = {
+                    resourceUrl: '',
+                    language: 'en',
+                    fallBackLanguage: 'en',
+                    features: ['']
+                };
 
-			this.setTranslateConfig = function (aTranslateConfig) {
-				translateConfig = aTranslateConfig;
-				var staticFiles = [];
-				var language = aTranslateConfig.language || 'en';
-				var fallBackLanguage = aTranslateConfig.fallBackLanguage || 'en';
-				if (aTranslateConfig.features) {
-					for (var i = 0; i < aTranslateConfig.features.length; i++) {
-						var featureFile = aTranslateConfig.resourceUrl + "/" + aTranslateConfig.features[i] + "_";
-						staticFiles.push({
-							prefix: featureFile,
-							suffix: '.json'
-						});
-					}
-				}
-				$translateProvider.useStaticFilesLoader({
-					files: staticFiles
-				});
+                this.setTranslateConfig = function (aTranslateConfig) {
+                    translateConfig = aTranslateConfig;
+                    var staticFiles = [];
+                    var language = aTranslateConfig.language || 'en';
+                    var fallBackLanguage = aTranslateConfig.fallBackLanguage || 'en';
+                    if (aTranslateConfig.features) {
+                        for (var i = 0; i < aTranslateConfig.features.length; i++) {
+                            var featureFile = aTranslateConfig.resourceUrl + "/" + aTranslateConfig.features[i] + "_";
+                            staticFiles.push({
+                                prefix: featureFile,
+                                suffix: '.json'
+                            });
+                        }
+                    }
+                    $translateProvider.useStaticFilesLoader({
+                        files: staticFiles
+                    });
 
-				$translateProvider
-						.useSanitizeValueStrategy('sanitizeParameters')
-						.preferredLanguage(language)
-						.fallbackLanguage(fallBackLanguage);
-				$translateProvider.forceAsyncReload(true);
-			};
+                    $translateProvider
+                        .useSanitizeValueStrategy('sanitizeParameters')
+                        .preferredLanguage(language)
+                        .fallbackLanguage(fallBackLanguage);
+                    $translateProvider.forceAsyncReload(true);
+                };
 
-			this.$get = [function () {
-					return $translateProvider;
+                this.$get = [function () {
+                    return $translateProvider;
 				}];
 		}]);
 }());
 
 
 (function () {
-	"use strict";
-	angular.module('com.synectiks.eskaySoft')
-	.provider("eskaySoftRouter", ["$stateProvider",
-	
+    "use strict";
+    angular.module('com.synectiks.eskaySoft')
+        .provider("eskaySoftRouter", ["$stateProvider",
+
 		function ($stateProvider) {
-			
-			this.setStateConfig = function () {
-				
-				var statesList=[
-					{
-						name:'home',
-						url: '/',
-						templateUrl: 'public/templates/login.html',
-						controller: 'loginInfoController'
+
+                this.setStateConfig = function () {
+
+                    var statesList = [
+                        {
+                            name: 'accountsInformation',
+                            url: '/accountsInformation',
+                            templateUrl: 'public/templates/accountsInformation.tbl.html',
+                            controller: 'loginInfoController'
 					}, {
-						name:'schedule',
-						url: '/schedule',
-						templateUrl: 'public/templates/schedule.html',
-						controller: 'scheduleController'
+                            name: 'home',
+                            url: '/',
+                            templateUrl: 'public/templates/login.html',
+                            controller: 'loginInfoController'
 					}, {
-						name:'statesInfo',
-						url: '/statesInfo',
-						templateUrl: 'public/templates/statesInfo.html',
-						controller: 'statesInfoController'
+                            name: 'schedule',
+                            url: '/schedule',
+                            templateUrl: 'public/templates/schedule.html',
+                            controller: 'scheduleController'
 					}, {
-						name:'bankInfo',
-						url: '/bankInfo',
-						templateUrl: 'public/templates/bankInfo.html',
-						controller: 'bankInfoController',
-						controllerAs: 'vm'
+                            name: 'statesInfo',
+                            url: '/statesInfo',
+                            templateUrl: 'public/templates/statesInfo.html',
+                            controller: 'statesInfoController'
 					}, {
-						name:'districtsInfo',
-						url: '/districtsInfo',
-						templateUrl: 'public/templates/districtsInfo.html',
-						controller: 'districtsInfoController'
+                            name: 'bankInfo',
+                            url: '/bankInfo',
+                            templateUrl: 'public/templates/bankInfo.html',
+                            controller: 'bankInfoController',
+                            controllerAs: 'vm'
 					}, {
-						name:'businessExeInfo',
-						url: '/businessExeInfo',
-						templateUrl: 'public/templates/businessExeInfo.html',
-						controller: 'businessExeInfoController'
+                            name: 'districtsInfo',
+                            url: '/districtsInfo',
+                            templateUrl: 'public/templates/districtsInfo.html',
+                            controller: 'districtsInfoController'
 					}, {
-						name:'areasInfo',
-						url: '/areasInfo',
-						templateUrl: 'public/templates/areasInfo.html',
-						controller: 'areasInfoController'
+                            name: 'businessExeInfo',
+                            url: '/businessExeInfo',
+                            templateUrl: 'public/templates/businessExeInfo.html',
+                            controller: 'businessExeInfoController'
 					}, {
-						name:'login',
-						url: '/login',
-						templateUrl: 'public/templates/login.html',
-						controller: 'loginInfoController'
+                            name: 'areasInfo',
+                            url: '/areasInfo',
+                            templateUrl: 'public/templates/areasInfo.html',
+                            controller: 'areasInfoController'
 					}, {
-						name:'registration',
-						url: '/registration',
-						templateUrl: 'public/templates/registration.html',
-						controller: 'registrationInfoController'
+                            name: 'login',
+                            url: '/login',
+                            templateUrl: 'public/templates/login.html',
+                            controller: 'loginInfoController'
 					}, {
-						name:'contact',
-						url: '/contact',
-						templateUrl: 'public/templates/contact.html',
-						controller: 'contactController'
+                            name: 'registration',
+                            url: '/registration',
+                            templateUrl: 'public/templates/registration.html',
+                            controller: 'registrationInfoController'
 					}, {
-						name:'registrationForm',
-						url: '/registrationForm',
-						templateUrl: 'public/templates/registrationForm.html',
-						controller: 'registrationFormController'
+                            name: 'contact',
+                            url: '/contact',
+                            templateUrl: 'public/templates/contact.html',
+                            controller: 'contactController'
 					}, {
-						name:'component',
-						url: '/component',
-						templateUrl: 'public/templates/component.html',
-						controller: ''
+                            name: 'registrationForm',
+                            url: '/registrationForm',
+                            templateUrl: 'public/templates/registrationForm.html',
+                            controller: 'registrationFormController'
 					}, {
-						name:'module',
-						url: '/module',
-						templateUrl: 'public/templates/newmodule.html',
-						controller: ''
+                            name: 'component',
+                            url: '/component',
+                            templateUrl: 'public/templates/component.html',
+                            controller: ''
 					}, {
-						name:'provider',
-						url: '/provider',
-						templateUrl: 'public/templates/provider.html',
-						controller: 'mainController'
+                            name: 'module',
+                            url: '/module',
+                            templateUrl: 'public/templates/newmodule.html',
+                            controller: ''
 					}, {
-						name:'subschedule',
-						url: '/subschedule',
-						templateUrl: 'public/templates/subSchedules.html',
-						controller: 'subScheduleController'
-					},  {
-						name:'manufacturer',
-						url: '/manufacturer',
-						templateUrl: 'public/templates/manufacturer.html',
-						controller: 'manufacturerController'
+                            name: 'provider',
+                            url: '/provider',
+                            templateUrl: 'public/templates/provider.html',
+                            controller: 'mainController'
 					}, {
-						name:'companyGroup',
-						url: '/companyGroup',
-						templateUrl: 'public/templates/companyGroup.html',
-						controller: 'companyGroupController'
+                            name: 'subschedule',
+                            url: '/subschedule',
+                            templateUrl: 'public/templates/subSchedules.html',
+                            controller: 'subScheduleController'
 					}, {
-						name:'productGroup',
-						url: '/productGroup',
-						templateUrl: 'public/templates/productGroup.html',
-						controller: 'productGroupController'
+                            name: 'manufacturer',
+                            url: '/manufacturer',
+                            templateUrl: 'public/templates/manufacturer.html',
+                            controller: 'manufacturerController'
 					}, {
-						name:'companies',
-						url: '/companies',
-						templateUrl: 'public/templates/companies.html',
-						controller: 'companiesController'
+                            name: 'companyGroup',
+                            url: '/companyGroup',
+                            templateUrl: 'public/templates/companyGroup.html',
+                            controller: 'companyGroupController'
 					}, {
-						name:'productCategory',
-						url: '/productCategory',
-						templateUrl: 'public/templates/productCategory.html',
-						controller: 'productCategoryController'
+                            name: 'productGroup',
+                            url: '/productGroup',
+                            templateUrl: 'public/templates/productGroup.html',
+                            controller: 'productGroupController'
 					}, {
-						name:'product',
-						url: '/product',
-						templateUrl: 'public/templates/product.html',
-						controller: 'productController'
+                            name: 'companies',
+                            url: '/companies',
+                            templateUrl: 'public/templates/companies.html',
+                            controller: 'companiesController'
+					}, {
+                            name: 'productCategory',
+                            url: '/productCategory',
+                            templateUrl: 'public/templates/productCategory.html',
+                            controller: 'productCategoryController'
+					}, {
+                            name: 'product',
+                            url: '/product',
+                            templateUrl: 'public/templates/product.html',
+                            controller: 'productController'
 					}
 			];
-			angular.forEach(statesList, function(state) {
-			   $stateProvider
-                .state(state.name, {
-                    url: state.url,
-                    templateUrl: state.templateUrl,
-                    controller: state.controller,
-                    controllerAs: 'vm'
-                });
-			});
-				
-			};
+                    angular.forEach(statesList, function (state) {
+                        $stateProvider
+                            .state(state.name, {
+                                url: state.url,
+                                templateUrl: state.templateUrl,
+                                controller: state.controller,
+                                controllerAs: 'vm'
+                            });
+                    });
 
-			this.$get = [function () {
-					return $stateProvider;
+                };
+
+                this.$get = [function () {
+                    return $stateProvider;
 				}];
 		}]);
-	}());
+}());
