f9da9db94c66d1e4890c2117abe2e4077d30dcc4
Sinu <vijay.kesanupalli@synectiks.com>
2018-01-06 18:18:43 +0530

    adding accountsInformationController.js

---
+++ src/controller/accountsInformationController.js
@@ -0,0 +1,12 @@
+(function () {
+
+    'use strict';
+
+
+    angular.module('com.synectiks.eskaySoft')
+        .controller('accountsInformationController', ['$scope', function ($scope) {
+            var vm = this; // jshint ignore:line
+
+    }])
+
+})();
