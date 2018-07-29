4859b3558dc1141960e54dac7b1b943fce05625e
synectiks <vijay.kesanupalli@synectiks.com>
2018-07-29 12:13:43 -0400

    Subschedule fix

--- src/controller/subScheduleController.js
+++ src/controller/subScheduleController.js
@@ -5,7 +5,22 @@
             var vm = this; // jshint ignore:line
 			vm.scheduleNameArr=[];
 			vm.subScheduleIndexArr=[];
-			
+
+			vm.getScheduleNameArr = function () {
+		        commonLoaderService.load_Data(null, 'https://eskaysoft.synectiks.com/api/v1/schedules/', 'GET', null).then(function (scheduleData) {
+					if(scheduleData.length>0){
+						angular.forEach(scheduleData, function(item){
+							var scheduleObj={};
+							scheduleObj.scheduleId =item.id;
+							scheduleObj.scheduleName =item.scheduleName;
+							vm.scheduleNameArr.push(scheduleObj);
+						});
+					}
+                }, function (error) { // jshint ignore:line
+                    console.log("error", error);
+                });
+            };
+
 	        vm.search = function () {
                 vm.normalScreen = true;
 				vm.hiddenColArr=['subScheduleId','scheduleId'];
@@ -41,7 +56,25 @@
 					}
 				});
 			};
-             vm.search();
+			
+			vm.create = function () {
+                var reqobj = {
+                    "subScheduleName": vm.subScheduleName,
+                    "subScheduleIndex": vm.subScheduleIndex,
+                    "scheduleId": vm.scheduleNameValue
+                };
+				commonLoaderService.load_Data(reqobj, "https://eskaysoft.synectiks.com/api/v1/subschedules/", "POST", null).then(function(data) {
+				vm.getScheduleNameArr();
+            vm.search();
+				 }, function (error) { // jshint ignore:line
+				console.log("error", error);
+                });
+                vm.messageContainer = true;
+                vm.errorMessage = "Schedule saved.";
+            };
+			
+			vm.getScheduleNameArr();
+            vm.search();
             
      }]);
 })();
